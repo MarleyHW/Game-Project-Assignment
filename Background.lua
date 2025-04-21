05d06f39fe54159aff4ecf4732fa1ca9f7ec4f09
MarleyHW <maholt-williams@loyola.edu>
2025-04-21 13:28:13 -0400

    Starting the background for the game

---
+++ Background.lua
@@ -0,0 +1,6 @@
+local Class = require "hump.class"
+local bgSky = love.graphics.newImage("bg/sky.png")
+local bgClouds = love.graphics.newImage("bg/clouds.png")
+local bgBeach = love.graphics.newImage("bg/beach.png")
+local bgWave = love.graphics.newImage("bg/wave.png")
+local bgFoam = love.graphics.newImage("bg/foam.png")
